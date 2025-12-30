55d6e2a6be529fe34f1a539445d907341a19c066
darshanscodesoftwares <darshan.scodesoftwares@gmail.com>
2025-12-30 16:39:13 +0530

    update:37

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/", 
+//   server:{
+//     host:"0.0.0.0"
+//   }
+// })
+
 export default defineConfig({
   plugins: [react()],
-  base: "/", 
-  server:{
-    host:"0.0.0.0"
-  }
-})
+  base: "/",   // ✅ required
+});
+
